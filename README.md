dec4f0152114d9e0af9c95c4271b100678b93066
devcodewak <ubuntu7it@gmail.com>
2017-06-15 00:47:19 +0800

    gost c2i

---
+++ README.md
@@ -0,0 +1,13 @@
+### gost 2.4 修改版，用于openshift v3 c2i 方式部署
+
+  
+##### 部署说明
+参看：https://github.com/amsokol/openshift-golang-template  
+注意：目前该模版已支持命令行参数及配置文件  
+  
+ 
+##### thanks:  
+	https://github.com/ginuerzh/gost  
+	https://github.com/amsokol/openshift-golang-template  
+
+	
---
+++ cmd/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"runtime"
+	"sync"
+
+	"github.com/ginuerzh/gost"
+	"github.com/golang/glog"
+	"golang.org/x/net/http2"
+)
+
+var (
+	options struct {
+		ChainNodes, ServeNodes flagStringList
+	}
+)
+
+func init() {
+	var (
+		configureFile string
+		printVersion  bool
+	)
+
+	flag.StringVar(&configureFile, "C", "", "configure file")
+	flag.Var(&options.ChainNodes, "F", "forward address, can make a forward chain")
+	flag.Var(&options.ServeNodes, "L", "listen address, can listen on multiple ports")
+	flag.BoolVar(&printVersion, "V", false, "print version")
+	flag.Parse()
+
+	if err := loadConfigureFile(configureFile); err != nil {
+		glog.Fatal(err)
+	}
+
+	if glog.V(5) {
+		http2.VerboseLogs = true
+	}
+
+	if flag.NFlag() == 0 {
+		flag.PrintDefaults()
+		return
+	}
+
+	if printVersion {
+		fmt.Fprintf(os.Stderr, "gost %s (%s)\n", gost.Version, runtime.Version())
+		return
+	}
+
+	if len(options.ServeNodes) != 0 {
+		fmt.Printf("[M] gost   : %s (%s)\n", gost.Version, runtime.Version())
+		fmt.Printf("[M] Server : %v\n", options.ServeNodes)
+	}
+}
+
+func main() {
+	chain := gost.NewProxyChain()
+	if err := chain.AddProxyNodeString(options.ChainNodes...); err != nil {
+		fmt.Printf("[-] AddProxyNodeString: %v\n", err)
+		glog.Fatal(err)
+	}
+	chain.Init()
+
+	var wg sync.WaitGroup
+	for _, ns := range options.ServeNodes {
+		serverNode, err := gost.ParseProxyNode(ns)
+		if err != nil {
+			fmt.Printf("[-] ParseProxyNode: %v\n", err)
+			glog.Fatal(err)
+		}
+
+		glog.Info(serverNode)
+
+		wg.Add(1)
+		go func(node gost.ProxyNode) {
+			defer wg.Done()
+			server := gost.NewProxyServer(node, chain)
+			errsvr := server.Serve()
+			fmt.Printf("[-] server.Serve: %v\n", errsvr)
+			glog.Fatal(errsvr)
+		}(serverNode)
+	}
+	wg.Wait()
+	fmt.Println("[-] exit.")
+}
+
+func loadConfigureFile(configureFile string) error {
+	if configureFile == "" {
+		return nil
+	}
+	content, err := ioutil.ReadFile(configureFile)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(content, &options); err != nil {
+		return err
+	}
+	return nil
+}
+
+type flagStringList []string
+
+func (this *flagStringList) String() string {
+	return fmt.Sprintf("%s", *this)
+}
+func (this *flagStringList) Set(value string) error {
+	*this = append(*this, value)
+	return nil
+}
